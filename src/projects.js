934bb3967d8824b46a110446fbdf895e79678e20
jared <jaredgold@proton.me>
2023-03-31 19:31:49 -0400

    create form to select project, form will print to DOM correct group of todos

--- src/projects.js
+++ src/projects.js
@@ -1,5 +1,5 @@
 import { projectView, projectList } from './logic';
-
+import { todoAddDisplay } from './dom-workers';
 export default function viewProjects() {
   const content = document.querySelector('.content');
   const projectContainer = document.createElement('div');
@@ -8,6 +8,7 @@ export default function viewProjects() {
   content.appendChild(projectContainer);
 
   const projectFormContainer = document.createElement('form');
+  projectFormContainer.classList.add('project-display-form')
   content.appendChild(projectFormContainer);
 
   const projectFormLabel = document.createElement('label');
@@ -28,7 +29,36 @@ export default function viewProjects() {
       projectOption.textContent = `${project}`;
       projectFormSelect.appendChild(projectOption);
     })
+  const projectFormSubmit = document.createElement('input');
+  projectFormSubmit.setAttribute('type', 'submit');
+  projectFormSubmit.setAttribute('value', 'Enter');
+  projectFormSubmit.classList.add('project-entry-btn');
+  projectFormContainer.appendChild(projectFormSubmit);
+  (function projectListListener() {
+    const projectListEnterBtn = document.querySelector('.project-entry-btn');
+  
+    // projectListEnterBtn.forEach((btn) => {
+    //   if (btn.getAttribute('listener') === 'true') return;
+    //   btn.setAttribute('listener', 'true');
+    projectListEnterBtn.addEventListener('click', (e) => {
+        e.preventDefault();
+        console.log(projectFormSelect.value); // this is giving choice of select from submit btn
+        // should call something, which the end result is it will display everything matching this value
+        console.log(projectView(projectFormSelect.value)); // gets array of right projects
+        const selectedTodoGroup = projectView(projectFormSelect.value);
+        addGroup(selectedTodoGroup);
+
+        // now i need to take that array, foreach on it, with add display on each item.
+      });
+  }())
+
+  function addGroup(todoArray) {
+    todoArray.forEach((item) => {
+      todoAddDisplay(item)})
+  }
     
   }
+
+
   return { projectOptionCreate }
 }
